9f6fc93b6a632085209a4e32e2c4acd6d82a25cf
PedroOrellana98 <porellanaj@est.ups.edu.ec>
2020-05-10 11:25:55 -0500

    Commit validacion fecha

--- js/formulario.js
+++ js/formulario.js
@@ -1,20 +1,59 @@
-function validarTexto(){
+function validarNombre(){
 
     nombre = document.getElementById("txtNombre").value;
     nombreS = nombre.replace(/ /g, "");
 
     if (isNaN(nombre) && (nombre != nombreS)) {
         document.getElementById('txtNombre').style.color = "black";
-        console.log("Con espacios")
+        return true;
     }else{
         document.getElementById('txtNombre').style.color = "red";
-        console.log("Sin espacios")
+        return false;
+    }
+}
+
+function validarApellido(){
+
+    apellido = document.getElementById("txtApellido").value;
+    apellidoS = apellido.replace(/ /g, "");
+
+    if (isNaN(apellido) && (apellido != apellidoS)) {
+        document.getElementById('txtApellido').style.color = "black";
+        return true;
+    }else{
+        document.getElementById('txtApellido').style.color = "red";
+        return false;
     }
 }
 
 function validarTelefono(){
-    
+
+    telefono = document.getElementById("txtTelefono").value;
+
+    if (isNaN(telefono) || telefono.length > 10) {
+        document.getElementById('txtTelefono').style.color = "red";
+        return false;
+    }else{
+        document.getElementById('txtTelefono').style.color = "black";
+        return true;
+    }
 }
+
+function validarFecha(){
+
+    var array = document.getElementById('txtFecha').value.split('/');
+    var fecha = new Date(array[2], array[1], array[0]);
+    if (array.length == 3 && fecha && array[0] == fecha.getDate() && array[1] == fecha.getMonth() && array[2] == fecha.getFullYear()) {
+        document.getElementById('txtFecha').style.color = "black";
+        return false;
+    } else {
+        document.getElementById('txtFecha').style.color = "red";
+        return true;
+    }
+}
+
+
+
 /*function val_numero(string) {
     var out = '';
     var filtro = '1234567890';
